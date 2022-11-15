26945f2da30c73c48bb07b3cde6b076ec83dcf9b
Xiangchong Li <mr.superonion@hotmail.com>
2022-11-15 13:14:13 -0500

    commit

--- tests/test_shapelets_shear.py
+++ tests/test_shapelets_shear.py
@@ -53,22 +53,19 @@ def test_g1():
     )
     np.testing.assert_array_almost_equal(
         res_00, out[:, colnames.index("fpfs_M00")],
-        decimal=6,
+        decimal=5,
     )
     np.testing.assert_array_almost_equal(
         res_20, out[:, colnames.index("fpfs_M20")],
         decimal=5,
     )
-    print(res_20-out[:, colnames.index("fpfs_M20")])
-    print(res_20)
     np.testing.assert_array_almost_equal(
         res_22c, out[:, colnames.index("fpfs_M22c")],
-        decimal=6,
+        decimal=5,
     )
     np.testing.assert_array_almost_equal(
-        np.zeros(ndata),
-        out[:, colnames.index("fpfs_M22s")],
-        decimal=6,
+        np.zeros(ndata), out[:, colnames.index("fpfs_M22s")],
+        decimal=5,
 
     )
     return
@@ -90,20 +87,20 @@ def test_g2():
         * (data2[:, colnames.index("fpfs_M00")] - data2[:, colnames.index("fpfs_M40")])
     )
     np.testing.assert_array_almost_equal(
-        res_00,
-        out[:, colnames.index("fpfs_M00")],
-        decimal=6,
-        )
-    np.testing.assert_array_almost_equal(res_20,
-        out[:, colnames.index("fpfs_M20")],
-        decimal=6,
+        res_00, out[:, colnames.index("fpfs_M00")],
+        decimal=5,
         )
     np.testing.assert_array_almost_equal(
-        np.zeros(ndata), out[:, colnames.index("fpfs_M22c")]
+        res_20, out[:, colnames.index("fpfs_M20")],
+        decimal=5,
+        )
+    np.testing.assert_array_almost_equal(
+        np.zeros(ndata), out[:, colnames.index("fpfs_M22c")],
+        decimal=5,
         )
     np.testing.assert_array_almost_equal(res_22s,
         out[:, colnames.index("fpfs_M22s")],
-        decimal=6,
+        decimal=5,
         )
     return
 
