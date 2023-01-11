9905db1a3642a093b2423e6f4a7bb7ed62b75e6c
Xiangchong Li <mr.superonion@hotmail.com>
2023-01-11 11:05:25 +0900

    add docs

--- docs/conf.py
+++ docs/conf.py
@@ -13,17 +13,14 @@
 
 import os
 import sys
-import sphinx_rtd_theme
+sys.path.insert(0, os.path.abspath('../fpfs'))
+
 import fpfs
-
-sys.path.insert(0, os.path.abspath("../fpfs"))
-
-
 # -- Project information -----------------------------------------------------
 
-project = "lensPT"
-copyright = "2022, Xiangchong Li"
-author = "Xiangchong Li"
+project = 'ImPt'
+copyright = '2022, Xiangchong Li'
+author = 'Xiangchong Li'
 
 # The full version, including alpha/beta/rc tags
 release = fpfs.__version__
@@ -34,21 +31,28 @@ release = fpfs.__version__
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    "sphinx.ext.autodoc",
-    "sphinx.ext.mathjax",
-    "sphinx.ext.viewcode",
-    "sphinx.ext.autosectionlabel",
-    "sphinx.ext.napoleon",
-    "sphinx.ext.autosummary",
-    "sphinx.ext.intersphinx",
-    "myst_parser",
+    'sphinx.ext.autodoc',
+    'sphinx.ext.mathjax',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.autosectionlabel',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.intersphinx',
+    "sphinx_copybutton",
+    "sphinx_book_theme",
+    "myst_nb",
 ]
 
 intersphinx_mapping = {
-    "python": ("https://docs.python.org", None),
-    "numpy": ("http://docs.scipy.org/doc/numpy", None),
-    "scipy": ("http://docs.scipy.org/doc/scipy/reference", None),
-    "matplotlib": ("http://matplotlib.org/stable", None),
+    'python': ('https://docs.python.org', None),
+    'numpy': ('http://docs.scipy.org/doc/numpy', None),
+    'scipy': ('http://docs.scipy.org/doc/scipy/reference', None),
+    'matplotlib': ('http://matplotlib.org/stable', None),
+    "myst": ("https://myst-parser.readthedocs.io/en/latest/", None),
+    "markdown_it": ("https://markdown-it-py.readthedocs.io/en/latest", None),
+    "nbclient": ("https://nbclient.readthedocs.io/en/latest", None),
+    "nbformat": ("https://nbformat.readthedocs.io/en/latest", None),
+    "sphinx": ("https://www.sphinx-doc.org/en/master", None),
 }
 
 autosectionlabel_prefix_document = True
@@ -66,7 +70,7 @@ napoleon_use_rtype = False
 napoleon_preprocess_types = False
 napoleon_type_aliases = None
 napoleon_attr_annotations = True
-napoleon_custom_sections = [("Returns", "params_style")]
+napoleon_custom_sections = [('Returns', 'params_style')]
 
 # Add any paths that contain templates here, relative to this directory.
 # templates_path = ['_templates']
@@ -74,58 +78,73 @@ napoleon_custom_sections = [("Returns", "params_style")]
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 source_suffix = {
-    ".rst": "restructuredtext",
-    ".md": "markdown",
+    '.rst': 'restructuredtext',
+    '.myst': 'myst-nb',
+    '.ipynb': 'myst-nb',
 }
+master_doc = 'index'
+nb_merge_streams = True
 
-master_doc = "index"
 
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "sphinx_rtd_theme"
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+# html_theme = 'sphinx_rtd_theme'
+# html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+html_theme = "sphinx_book_theme"
+html_theme_options = {
+    "github_url": "https://github.com/executablebooks/myst-nb",
+    "repository_url": "https://github.com/executablebooks/myst-nb",
+    "repository_branch": "master",
+    "home_page_in_toc": True,
+    "path_to_docs": "docs",
+    "show_navbar_depth": 1,
+    "use_edit_page_button": True,
+    "use_repository_button": True,
+    "use_download_button": True,
+    "launch_buttons": {
+        "binderhub_url": "https://mybinder.org",
+        "notebook_interface": "classic",
+    },
+}
 
-
-def setup(app):
-    app.add_css_file("xtheme.css")
-
+# def setup(app):
+#     app.add_css_file('xtheme.css')
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["_static"]
+html_static_path = ['_static']
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = "sphinx"
+pygments_style = 'sphinx'
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.
-default_role = "any"
+default_role = 'any'
 
 # myst
 myst_enable_extensions = [
-    "dollarmath",
+    "colon_fence",
     "amsmath",
+    "dollarmath",
     "deflist",
     "fieldlist",
+    "tasklist",
+    "smartquotes",
     "html_admonition",
     "html_image",
-    "colon_fence",
-    "smartquotes",
-    "replacements",
-    "strikethrough",
-    "substitution",
-    "tasklist",
 ]
 myst_number_code_blocks = ["typescript"]
 myst_heading_anchors = 2
 myst_footnote_transition = True
 myst_dmath_double_inline = True
 
-htmlhelp_basename = "fpfs_doc"
+htmlhelp_basename = 'fpfs_doc'
+
+
---
+++ docs/overview.md
@@ -0,0 +1,32 @@
+# Overview
+
+FPFS is an open-source software for fast and accurate shear estimation.
+
+## Basic installation
+
+For stable version:
+```shell
+pip install impt
+```
+
+Or clone the repository:
+```shell
+git clone https://github.com/mr-superonion/impt.git
+cd impt
+pip install -e .
+```
+
+
+## Reference
+The following papers are ready to be cited if you find any of these papers
+interesting or use the pipeline. Comments are welcome.
+
+
+## Developers
+
++ Xiangchong Li (xiangchl at andrew.cmu.edu)
+
+If you have any trouble installing or using the code, or find a bug, or have a
+suggestion for a new feature, please open up an Issue on our [GitHub
+repository](https://github.com/mr-superonion/impt). We also accept pull
+requests if you have something you’d like to contribute to the code base.
