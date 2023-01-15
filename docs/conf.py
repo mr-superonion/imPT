a73907ae2da56a432d7272ef1a1b00447db00081
Xiangchong Li <mr.superonion@hotmail.com>
2023-01-16 07:00:35 +0900

    commit

--- docs/conf.py
+++ docs/conf.py
@@ -10,12 +10,14 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
+# flake8: noqa
 
 import os
 import sys
-import impt
 
 sys.path.insert(0, os.path.abspath("../impt"))
+
+import impt
 
 
 # -- Project information -----------------------------------------------------
